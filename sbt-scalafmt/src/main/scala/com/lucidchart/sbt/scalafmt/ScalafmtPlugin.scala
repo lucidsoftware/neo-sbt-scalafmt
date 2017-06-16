fa1771367782b3913ffcdabfefe4fa4a1c747522
Paul Draper <paul@lucidchart.com>
2017-06-16 14:23:22 -0600

    Add root *.scala sources if sourcesInBase is true

--- sbt-scalafmt/src/main/scala/com/lucidchart/sbt/scalafmt/ScalafmtPlugin.scala
+++ sbt-scalafmt/src/main/scala/com/lucidchart/sbt/scalafmt/ScalafmtPlugin.scala
@@ -2,10 +2,21 @@ package com.lucidchart.sbt.scalafmt
 
 import com.lucidchart.sbt.scalafmt.ScalafmtCorePlugin.autoImport._
 import sbt._
+import sbt.Keys._
 
 object ScalafmtPlugin extends AutoPlugin {
 
-  override val projectSettings = Seq(Compile, Test).flatMap(inConfig(_)(scalafmtSettings))
+  override val projectSettings = Seq(Compile, Test).flatMap(inConfig(_)(scalafmtSettings)) ++
+    inConfig(Compile)(
+      inTask(scalafmt)(
+        Seq(
+          sources ++= {
+            val finder = (baseDirectory.value * includeFilter.value) --- (baseDirectory.value * excludeFilter.value)
+            if (sourcesInBase.value) finder.get else Seq.empty
+          }
+        )
+      )
+    )
 
   override val requires = ScalafmtCorePlugin
 
