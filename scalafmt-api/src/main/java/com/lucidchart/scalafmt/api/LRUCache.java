88c03b4c0f75b0a4afb70a4ca7bfa07695dc1c1e
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2017-10-23 11:59:09 +0200

    Add a little more documenation

--- scalafmt-api/src/main/java/com/lucidchart/scalafmt/api/LRUCache.java
+++ scalafmt-api/src/main/java/com/lucidchart/scalafmt/api/LRUCache.java
@@ -5,12 +5,15 @@ import java.util.Map;
 
 /**
  * A very simple LRU cache based an a JVM collection type.
+ *
+ * http://chriswu.me/blog/a-lru-cache-in-10-lines-of-java/
  */
 
 public class LRUCache<K, V> extends LinkedHashMap<K, V> {
     private int cacheSize;
 
     public LRUCache(int cacheSize) {
+        // Magic happens here: the third parameter specifies that the keys are ordered on access not insertion.
         super(16, 0.75f, true);
         this.cacheSize = cacheSize;
     }
