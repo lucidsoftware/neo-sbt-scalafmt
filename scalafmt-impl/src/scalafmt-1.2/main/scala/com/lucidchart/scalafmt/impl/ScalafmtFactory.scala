116e6f6b1b579674d2947fe0cc6447bde88ec530
Dale Wijnand <dale.wijnand@gmail.com>
2018-04-27 07:27:56 +0100

    Cross-build to Scalafmt 1.2

---
+++ scalafmt-impl/src/scalafmt-1.2/main/scala/com/lucidchart/scalafmt/impl/ScalafmtFactory.scala
@@ -0,0 +1,13 @@
+package com.lucidchart.scalafmt.impl
+
+import com.lucidchart.scalafmt.api
+import com.lucidchart.scalafmt.api.{Dialect, Scalafmtter}
+import java.util.function
+import org.scalafmt.config.Config
+import scala.meta.dialects
+
+final class ScalafmtFactory extends api.ScalafmtFactory {
+
+  def fromConfig(configString: String) = new Scalafmtter(Config.fromHoconString(configString, Option.empty).get)
+
+}
